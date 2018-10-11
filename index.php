d877b1e9ca896e487afb3711368032bc61d91eb7
Daywey <wmcompass@users.noreply.github.com>
2018-10-11 21:10:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 
   
 <body>
+ 
+<form action ="search.php" method="post">
+<input type="text" name ="key_word">
+<button type="button">Search</button>
+</form>
   
+ <br><br> 
  <form action="save_data.php" method ="post">
  姓氏: <input type="text" name="lastname">
  名字: <input type="text" name="firstname">
@@ -12,13 +18,10 @@
  <input type ="radio" name="gender" value="male”>Male<br>
  <input type ="radio" name="gender" value=female"Female<br>
  身分證字號: <input type ="text" name="personal_ID">
-   
-   
-   
+     
  <input type="submit">
  </form>
 
-  
   
 </body>
 </html>
