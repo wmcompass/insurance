da79a5e6ec7f726d02b27595de2f5f6c92d905d1
Daywey <wmcompass@users.noreply.github.com>
2018-10-11 20:38:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
-<form>
-  姓氏:<br>
-  <input type="text" name="lastname">
-  <br>
-  名字:<br>
-  <input type="text" name="firstname">
+
+  <form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+  
+  
+ <form action="enter customer profile" method ="post">
+ 姓氏: <input type="text" name="lastname">
+ 名字: <input type="text" name="firstname">
+   生日:<br>
+  <input type="text" name="birthdate">  
 </form>
