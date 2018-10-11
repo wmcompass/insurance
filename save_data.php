4b8b77af5022a82fdce5f88dc90ce252098361db
Daywey <wmcompass@users.noreply.github.com>
2018-10-11 21:41:51 +0800

    Create save_data.php

---
+++ save_data.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<?php
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$birth_date = $_POST[""]
+$personal_ID = $_POST["personal_ID"];
+
+
+
+
+?>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+
+
+</body>
+</html>
